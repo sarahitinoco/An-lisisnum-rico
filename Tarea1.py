2782573e4d0f82a9990c1edd79cb71d55f72c4e3
sarahitinoco <tu-email@ejemplo.com>
2025-02-13 11:11:30 -0600

    realicé cambios en la línea 4 y 32, añadí las líneas desde la 33 a la 49

--- Tarea1.py
+++ Tarea1.py
@@ -1,6 +1,7 @@
 #Ejercicio 1
 import math as mt
 
+"""
 f = float(input("Ingrese una temperatura en grado farenheit: "))
 c = (f-32)*(1.8)
 print("tu temperatura es :", c, "celsius")
@@ -29,5 +30,19 @@ print("el número complejo r es: ", r)
 q=5
 p=9
 print("el resultado es: ", complex(q, p))
-
+"""
 #Ejercicio 3
+from cmath import sin, sinh, exp, e, cos
+#1
+X= complex(input('Ingrese un número: '))
+#x1= float(X)
+print('sin(i ', X, ')=', sin(X*1j))
+print('sinh(', X, ')=', 1j*sinh(X))
+
+#2
+n=complex(input('Ingrese un número complejo:\n '))
+ne= exp(1j*n)
+print(f'su número es {ne}')
+print(f'Su número con la relación coseno + seno es {cos(n)+ 1j*sin(n)}')
+
+#Ejercicio 4
